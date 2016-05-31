105316229c83a2aaa005a3f0371028b4a972c011
Asgeir Stavik Hustad <askhus@online.no>
2016-05-31 13:20:36 +0200

    ID-krøll.

--- dropbox.cron.php
+++ dropbox.cron.php
@@ -25,7 +25,9 @@ $qry = new SQL("SELECT *,
 				`i`.`id` AS `id`,
 				`u`.`id` AS `user_id`,
 				`i`.`url` AS `url`,
-				`u`.`url` AS `user_url`
+				`u`.`url` AS `user_url`,
+				`i`.`insta_id` AS `insta_id`,
+				`u`.`insta_id` AS `insta_user_id`
 				FROM `ukm_insta_bilder` AS `i`
 					JOIN `ukm_insta_users` AS `u` 
 					ON (`i`.`user_id` = `u`.`id`)
