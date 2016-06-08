0ade371ac8ed3b7492ab31b1f5ecd64518aae544
Asgeir Stavik Hustad <askhus@online.no>
2016-06-08 15:52:30 +0200

    Typo

--- authenticate.php
+++ authenticate.php
@@ -8,7 +8,7 @@ require_once('UKMconfig.inc.php');
 
 use Httpful\Request;
 
-$CLIENT_ID = INSTAGRAM_CLIENT_ID;
+$INSTAGRAM_CLIENT_ID = INSTAGRAM_CLIENT_ID;
 
 $redirect_uri = 'http://insta.ukm.no/authenticated.php';
 $authentication_uri = 'https://api.instagram.com/oauth/authorize/?client_id='.$INSTAGRAM_CLIENT_ID.'&redirect_uri='.$redirect_uri.'&response_type=code';
