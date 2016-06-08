28f844950ae188dfaf57c7820b1a40678faf0963
Asgeir Stavik Hustad <askhus@online.no>
2016-06-08 17:12:46 +0200

    SQL-fiks

--- authenticated.php
+++ authenticated.php
@@ -28,7 +28,7 @@ $curl->post(array(
 $result = $curl->process($authorization_endpoint);
 
 $qry = new SQLins('ukm_insta_config', array('option_name' => 'access_token') );
-$qry->add($result->access_token);
+$qry->add('option_name', $result->access_token);
 $res = $qry->run();
 
 if ($res === false) {
