86c989c6bbe37fdfb10843b9f8b6d2d2a53f1682
Asgeir Stavik Hustad <askhus@online.no>
2016-06-08 17:21:53 +0200

    Correct scope

--- authenticate.php
+++ authenticate.php
@@ -11,6 +11,6 @@ use Httpful\Request;
 $INSTAGRAM_CLIENT_ID = INSTAGRAM_CLIENT_ID;
 $redirect_uri = INSTAGRAM_AUTHORIZATION_REDIRECT_URI;
 
-$authentication_uri = 'https://api.instagram.com/oauth/authorize/?client_id='.$INSTAGRAM_CLIENT_ID.'&redirect_uri='.$redirect_uri.'&response_type=code';
+$authentication_uri = 'https://api.instagram.com/oauth/authorize/?client_id='.$INSTAGRAM_CLIENT_ID.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=public_content';
 
 echo '<script>window.location.href = "' . $authentication_uri . '"</script>';
