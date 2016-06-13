951b50afd5afe848c856adb5c0cfa1f881c79570
Asgeir Stavik Hustad <askhus@online.no>
2016-06-13 11:27:04 +0200

    Privacy policy

---
+++ privacy_policy.php
@@ -0,0 +1,31 @@
+<?php
+
+?>
+<h>English</h>
+<b>Privacy policy - UKMs use of Instagram data</b>
+<p>
+UKM uses the Instagram API only to collect pictures posted with UKM-related hashtags (#javielskerukm etc). 
+</p>
+<p>
+During the festival week, these pictures are automatically printed and hung on the wall in our TV-studio and festival media office. All pictures are printed with the proper credit: username and full name (if available), as well as the caption of your picture.
+</p>
+<p>
+We will never save your login information, share it with others nor like/comment/share on your profile.
+</p>
+<p>
+If you’ve posted a picture with a hashtag we’re using and don’t want it to be printed or viewed in our systems, just pop us an email at support@ukm.no.
+</p>
+<h>Norsk</h>
+<b>UKMs bruk av Instagram-data</b>
+<p>
+UKM bruker Instagram sitt API kun for å samle bilder tagget med UKM-relaterte hashtagger (som #javielskerukm etc).
+</p>
+<p>
+Under festivaluken blir disse bildene automatisk skrevet ut og hengt opp på veggen i TV-studioet og redaksjonslokalet vårt. Alle bilder er skrevet ut med korrekt akkreditering, altså brukernavn og fullt navn hvis tilgjengelig, samt bildeteksten din.
+</p>
+<p>
+Vi vil aldri lagre innloggingsinformasjonen din, dele den med andre eller like/kommentere/dele ting på din profil. 
+</p>
+<p>
+Hvis du har lagt ut et bilde med en hashtag vi bruker og ikke vil ha den skrevet ut eller med i våre systemer, send oss en epost på support@ukm.no, så fikser vi det.
+<p>
